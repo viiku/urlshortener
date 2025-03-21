3f6d87c2f5a48bd4187bafc6f1f8fd4bf6bc2ef7
viiku <vivekvikkusingh@gmail.com>
2025-03-21 09:12:15 +0530

    initial commit

---
+++ src/test/java/com/viiku/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viiku.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
